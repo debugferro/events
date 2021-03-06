9747a8302f744d38fc226fc792979c29966dd131
Gabriel Fontes <gabrielferroroque@hotmail.com>
2021-03-05 21:49:50 -0300

    Added number to issue

--- octo_events/db/schema.rb
+++ octo_events/db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_06_004920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "events", force: :cascade do |t|
+    t.string "action"
+    t.bigint "issue_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["issue_id"], name: "index_events_on_issue_id"
+  end
+
+  create_table "issues", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "number"
+  end
+
+  add_foreign_key "events", "issues"
 end
